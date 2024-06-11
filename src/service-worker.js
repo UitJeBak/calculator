bcb52f91a2a1f21675d17aab763ba67676f73785
Thomas Bleijendaal <t.bleijendaal@wearetriple.com>
2024-06-11 21:53:07 +0200

    Service worker working

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,5 +1,35 @@
-self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        new Response("OK", { status: 200 })
+const cacheVersion = "v11";
+
+const addResourcesToCache = async (resources) => {
+    const cache = await caches.open(cacheVersion);
+    await cache.addAll(resources);
+};
+
+const putInCache = async (request, response) => {
+    const cache = await caches.open(cacheVersion);
+    await cache.put(request, response);
+};
+
+const cacheFirst = async (request) => {
+    const responseFromCache = await caches.match(request);
+    if (responseFromCache) {
+        return responseFromCache;
+    }
+    const responseFromNetwork = await fetch(request);
+    putInCache(request, responseFromNetwork.clone());
+    return responseFromNetwork;
+};
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        addResourcesToCache([
+            "/",
+            "/index.html",
+            "/manifest.json"
+        ]),
     );
+});
+
+self.addEventListener("fetch", async (event) => {
+    event.respondWith(cacheFirst(event.request));
 });
